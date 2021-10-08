7ff5dc3683fcd578a0c35d7fa880119e7bc11df9
Alejo Acevedo <acevedo@millionandup.com>
2021-10-08 10:27:01 -0500

    share url

--- controller.ts
+++ controller.ts
@@ -4,13 +4,14 @@ btnShare.addEventListener('click', shareFile);
 
 function shareFile () {
     console.log('Begin share');
-    let vcardBlob = new Blob(["test card text plain"], {type: "text/plain;charset=utf-16"});
-    let shareFile = [new File([vcardBlob], "card.txt", { type: "text/plain;charset=utf-16" })];
+    // let vcardBlob = new Blob(["test card text plain"], {type: "text/plain;charset=utf-16"});
+    // let shareFile = [new File([vcardBlob], "card.txt", { type: "text/plain;charset=utf-16" })];
 
     if (navigator.share ) {
         navigator.share( {
             text: "Contact",
-            files: shareFile
+            url: window.location.href
+            // files: shareFile
         })
         .catch(console.error);
 
