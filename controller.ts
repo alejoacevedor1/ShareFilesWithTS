f3bc33fe52a581869921a331430dbcbd39c0a2bd
Alejo Acevedo <acevedo@millionandup.com>
2021-10-08 10:25:18 -0500

    track in console

--- controller.ts
+++ controller.ts
@@ -3,6 +3,7 @@ let btnShare = document.getElementById('BtnShare');
 btnShare.addEventListener('click', shareFile);
 
 function shareFile () {
+    console.log('Begin share');
     let vcardBlob = new Blob(["test card text plain"], {type: "text/plain;charset=utf-16"});
     let shareFile = [new File([vcardBlob], "card.txt", { type: "text/plain;charset=utf-16" })];
 
@@ -12,5 +13,11 @@ function shareFile () {
             files: shareFile
         })
         .catch(console.error);
+
+        console.log('END share');
+
     } 
+    else{
+        console.log('No support share');
+    }
 }
