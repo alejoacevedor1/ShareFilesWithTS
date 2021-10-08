8f5484aa83bad7f0be0bf9150f0ef6dcd8079b85
Alejo Acevedo <acevedo@millionandup.com>
2021-10-08 10:15:36 -0500

    change type MIME

--- controller.ts
+++ controller.ts
@@ -4,7 +4,7 @@ btnShare.addEventListener('click', shareFile);
 
 function shareFile () {
     let vcardBlob = new Blob(["test card text plain"], {type: "text/plain;charset=utf-16"});
-    let shareFile = [new File([vcardBlob], "card.txt", { type: vcardBlob.type })];
+    let shareFile = [new File([vcardBlob], "card.txt", { type: "text/plain;charset=utf-16" })];
 
     if (navigator.share ) {
         navigator.share( {
