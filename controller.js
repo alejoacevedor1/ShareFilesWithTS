41149f58d9175f5ec1533504b1bf63dac50bc391
Alejo Acevedo <acevedo@millionandup.com>
2021-10-11 14:29:12 -0500

    add canShare

--- controller.js
+++ controller.js
@@ -7,27 +7,22 @@ function shareFile() {
     track.textContent = 'Begin share';
     var vcardBlob = new Blob(["test card text plain"], { type: "text/plain;charset=utf-16" });
     var shareFile = [new File([vcardBlob], "card.txt", { type: "text/plain;charset=utf-16" })];
-    var errorShare = false;
     var shareData = {};
     shareData.text = "Text share";
     shareData.title = "Title share";
-    shareData.files = shareFile;
+    shareData.url = window.location.href;
+    var navigator;
+    if (navigator.canShare && navigator.canShare({ files: shareFile })) {
+        shareData.files = shareFile;
+        delete shareData.url;
+        canFile.textContent = "Can share files";
+    }
+    else {
+        canFile.textContent = "No share files";
+    }
     if (navigator.share) {
         navigator.share(shareData)
-            .then(function () { return message.textContent = "Success"; })["catch"](function (error) {
-            console.error(error.message);
-            errorShare = true;
-            canFile.textContent = "No support share files";
-        });
-        if (errorShare) {
-            delete shareData.files;
-            shareData.url = window.location.href;
-            navigator.share(shareData)
-                .then(function () { return message.textContent = "Success"; })["catch"](function (error) {
-                console.log(error.message);
-                message.textContent = "Can not share:" + error.message;
-            });
-        }
+            .then(function () { return message.textContent = "Success"; })["catch"](function (error) { return console.error(error); });
         track.textContent = 'End share';
     }
     else {
--- controller.ts
+++ controller.ts
@@ -9,39 +9,35 @@ function shareFile () {
     track.textContent = 'Begin share';
     let vcardBlob = new Blob(["test card text plain"], {type: "text/plain;charset=utf-16"});
     let shareFile = [new File([vcardBlob], "card.txt", { type: "text/plain;charset=utf-16" })];
-    let errorShare: boolean = false;
 
     let shareData: ShareData = {};
     shareData.text = "Text share";
     shareData.title = "Title share";
-    shareData.files = shareFile;
+    shareData.url = window.location.href;
+    
+    interface ExtendNavigator extends Navigator {
+        canShare?: (data?: ShareData) => boolean;
+    }
+
+    let navigator: ExtendNavigator;
+
+    if (navigator.canShare && navigator.canShare({ files: shareFile }))
+    {
+        shareData.files = shareFile;
+        delete shareData.url;
+        canFile.textContent = "Can share files";
+    }
+    else{
+        canFile.textContent = "No share files";
+    }
+
 
     if (navigator.share) {
         navigator.share( shareData )
         .then(() => message.textContent = "Success")
-        .catch( error => 
-            {
-                console.error(error.message);
-                errorShare = true;
-                canFile.textContent = "No support share files"
-            }
-        )
-
-        if (errorShare)
-        {
-            delete shareData.files;
-            shareData.url = window.location.href;
-            navigator.share( shareData )
-            .then(() => message.textContent = "Success")
-            .catch(error => {
-                console.log(error.message);
-                message.textContent = "Can not share:" + error.message;
-            } )
-        }
+        .catch( error => console.error(error))
 
         track.textContent = 'End share';
-        
-
     } 
     else{
         alert('No support share');
